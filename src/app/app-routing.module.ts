ea9d47c5564bf272b511ae0162fc57518ee68797
Shubh soni <shubhsoni.dev@gmail.com>
2024-06-03 08:53:29 +0530

    enh:created page for tracking

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { RedirectComponent } from './redirect/redirect.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { TrackComponent } from './track/track.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/main', pathMatch: 'full' },
   { path: 'main', component: MainComponent },
   { path: 're/:id', component: RedirectComponent },
+  { path: 'tracking/:id', component: TrackComponent },
   { path: 'NotFound', component: NotFoundComponent },
 ];
 
